0b42fc7d0f49ea3c0b50916ffcf3552020771e68
Kseniia <inanast153@gmail.com>
2023-11-14 19:24:53 +0100

    Добавляет модуль, создающий рандомные комментарии под фотографиями

--- js/open-fullsize-picture.js
+++ js/open-fullsize-picture.js
@@ -1,5 +1,6 @@
 import { isEscapeKey, isEnterKey } from './util.js';
-import { picturesContainer, similarMiniatures, renderMiniatures } from './miniatures.js';
+import { picturesContainer, similarMiniatures, renderMiniatures } from './create-miniatures.js';
+import { renderComments } from './create-social-comments.js';
 
 const bigPictureContainer = document.querySelector('.big-picture');
 const bigPictureClose = bigPictureContainer.querySelector('.big-picture__cancel');
@@ -33,13 +34,13 @@ const renderGallery = () => {
     const miniatureId = parseInt(miniature.dataset.miniatureId, 10);
     const selectedPicture = similarMiniatures.find(({ id }) => id === miniatureId);
     renderFullsizePicture(selectedPicture);
+    renderComments(selectedPicture.comments);
 
     bigPictureContainer.classList.remove('hidden');
     document.body.classList.add('modal-open');
+    document.addEventListener('keydown', onDocumentKeydown);
     countComment.classList.add('hidden');
     commentsLoader.classList.add('hidden');
-
-    document.addEventListener('keydown', onDocumentKeydown);
   };
 
   const onOpenPictureClick = (evt) => {
