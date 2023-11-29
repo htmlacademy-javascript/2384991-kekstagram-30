cec2f2c3460e5de07e40a14752410aeedc6fd0c6
Kseniia <inanast153@gmail.com>
2023-11-29 19:41:43 +0100

    Исправляет ошибку в консоли

--- js/open-fullsize-picture.js
+++ js/open-fullsize-picture.js
@@ -1,3 +1,4 @@
+// open-fullsize-picture.js
 import { renderComments } from './create-social-comments.js';
 import { isEscapeKey } from './util.js';
 import { renderMiniatures } from './create-miniatures.js';
@@ -45,11 +46,17 @@ function openBigPicture(target, pictures) {
 
   const miniatureId = parseInt(miniature.dataset.miniatureId, 10);
   const selectedPicture = pictures.find(({ id }) => id === miniatureId);
+
+  if (!selectedPicture) {
+    return;
+  }
+
   renderFullsizePicture(selectedPicture);
   bigPictureContainer.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
 }
+
 
 const closeBigPicture = (pictures) => {
   bigPictureContainer.classList.add('hidden');
